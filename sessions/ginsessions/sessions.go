4c6d7acbffe41d703621df679b53536304d38e8b
George <obsidian.musicwork@gmail.com>
2025-01-26 14:39:45 +0300

    fix(ginsessions)!: make ginsessions compatible with SessionStore interface

--- sessions/ginsessions/sessions.go
+++ sessions/ginsessions/sessions.go
@@ -1,6 +1,8 @@
 package ginsessions
 
 import (
+	"context"
+
 	"github.com/39george/authpher"
 	ginadapter "github.com/39george/scs_gin_adapter"
 	"github.com/gin-gonic/gin"
@@ -10,22 +12,25 @@ type GinSessions struct {
 	Store *ginadapter.GinAdapter
 }
 
-func (gs GinSessions) Get(ctx *gin.Context, key string) authpher.Data {
-	d := gs.Store.Get(ctx, key)
-	switch d.(type) {
+func (gs GinSessions) Get(ctx context.Context, key string) authpher.Data {
+	c := ctx.(*gin.Context)
+	d := gs.Store.Get(c, key)
+	switch d := d.(type) {
 	case authpher.Data:
-		return d.(authpher.Data)
+		return d
 	default:
 		return authpher.Data{}
 	}
 }
-func (gs GinSessions) Set(ctx *gin.Context, k string, v authpher.Data) {
-	gs.Store.Put(ctx, k, v)
+func (gs GinSessions) Set(ctx context.Context, k string, v authpher.Data) {
+	c := ctx.(*gin.Context)
+	gs.Store.Put(c, k, v)
 }
 
 // Not possible in gin
-func (gs GinSessions) Save(ctx *gin.Context) {}
+func (gs GinSessions) Save(ctx context.Context) {}
 
-func (gs GinSessions) Clear(ctx *gin.Context) {
-	gs.Store.Destroy(ctx)
+func (gs GinSessions) Clear(ctx context.Context) {
+	c := ctx.(*gin.Context)
+	gs.Store.Destroy(c)
 }
